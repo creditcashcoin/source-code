7d50eb3f3f674e460767d2d33858df3933b4276b
Yugandhar Gangu <yugandhargangu@Yugandhars-Mac-mini.local>
2017-10-16 10:13:04 +0530

    source-code

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define CreditCashCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "CreditCashCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
